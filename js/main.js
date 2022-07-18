98741060157500f86716bdd2bc4180acb9ae2b52
Riccardo <lupinu.ric@gmail.com>
2022-07-18 16:15:24 +0200

    Add: Stampa dati in console + stampa dati a video

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const team = [
+let containerDom = document.getElementById('container');
+const teamArray = [
     {
         nome: 'Wayne Barnett',
         ruolo: 'Founder & CEO',
@@ -28,5 +29,31 @@ const team = [
         nome: 'Barbara Ramos',
         ruolo: 'Graphic Designer',
         immagine: 'barbara-ramos-graphic-designer.jpg'
-    },
-]
+    }
+];
+
+// Stampa in console i valori di nome, ruolo e immagine
+
+for (let key in teamArray) {
+    console.log(teamArray[key]);
+}
+
+for (i = 0; i < teamArray.length; i++) {
+    //Creo Card
+    let card = document.createElement('div');
+    card.classList.add('card');
+    //Aggiungo Card a containerDom
+    containerDom.append(card);
+    let nome = document.createElement('h2');
+    card.append(nome);
+    nome.append(teamArray[i].nome);
+    let ruolo = document.createElement('h3');
+    card.append(ruolo);
+    ruolo.append(teamArray[i].ruolo);
+    let immagine = document.createElement('h4');
+    card.append(immagine);
+    immagine.append(teamArray[i].immagine);
+    containerDom.append(card);
+}
+
+//+ ' ' + teamArray[i].ruolo + ' ' + teamArray[i].immagine);
